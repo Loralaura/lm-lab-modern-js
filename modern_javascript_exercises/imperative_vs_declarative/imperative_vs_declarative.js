fac0a317c237b36332950ff879bbe37eb08361ee
Laura Robinson <laura3918@hotmail.co.uk>
2023-10-18 18:17:06 +0100

    feat: Added solutions from JS intro session

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -1,13 +1,9 @@
 // Instructions can be found in imperative_vs_declarative.md
 
 export function longPasswords(passwords) {
-  let longPasswords = [];
+  const longPasswords = passwords.filter((password) => {
+    return password.length >= 9;
+  });
 
-  for (let i = 0; i < passwords.length; i++) {
-    const password = passwords[i];
-    if (password.length >= 9) {
-      longPasswords.push(password);
-    }
-  }
   return longPasswords;
 }
--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,14 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(...numbers) {
+  let total = 0;
+
+  numbers.forEach((number) => {
+    total += number;
+  });
+
+  return total;
 }
 
 add(1, 2, 3, 4, 5);
